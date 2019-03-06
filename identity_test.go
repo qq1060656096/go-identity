5b6aa5c90a346bbc7400de91152eafa682c07674
赵威杰 <1060656096@qq.com>
2019-03-06 12:51:33 +0800

    caa: 身份证解析完成

---
+++ identity_test.go
@@ -0,0 +1,44 @@
+package identity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var tests = map[string]string {
+	"18": "511521198602035558",
+	"15": "511521860203555",
+}
+
+// 测试解析15位身份证
+func TestNew15(t *testing.T) {
+	s := tests["15"]
+	id := New(s)
+	err := id.Parse()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, id.GetProvince(), "51")
+	assert.Equal(t, id.GetCity(), "15")
+	assert.Equal(t, id. GetArea(), "21")
+
+	assert.Equal(t, id.GetBirthday(), "19860203")
+	assert.Equal(t, id.GetSex(), SexMale)
+
+	assert.Equal(t, id.GetSequenceCode(), "555")
+	assert.Equal(t, id.GetVerifyCode(), "")
+}
+
+func TestNew18(t *testing.T) {
+	s := tests["18"]
+	id := New(s)
+	err := id.Parse()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, id.GetProvince(), "51")
+	assert.Equal(t, id.GetCity(), "15")
+	assert.Equal(t, id. GetArea(), "21")
+
+	assert.Equal(t, id.GetBirthday(), "19860203")
+	assert.Equal(t, id.GetSex(), SexMale)
+	assert.Equal(t, id.GetSequenceCode(), "555")
+	assert.Equal(t, id.GetVerifyCode(), "8")
+
+}
